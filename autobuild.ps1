40c7f04501ecb5d4321390676e9a8e1f16487f5c
Barış Genç <31003856+cervantes79@users.noreply.github.com>
2023-12-12 22:48:24 +0300

    bot

--- autobuild.ps1
+++ autobuild.ps1
@@ -8,13 +8,13 @@ $newVersion = "$($versionParts[0]).$($versionParts[1]).$newMinorVersion"
 
 # Yeni versiyon ile Docker imajlarını build et
 docker build -t nvrbox/autobot:$newVersion -t nvrbox/autobot:latest -f ./BotDockerfile . --no-cache
-docker build -t nvrbox/autoschedul:$newVersion -t nvrbox/autoschedul:latest -f ./SchDockerfile . --no-cache
+docker build -t nvrbox/autoschedule:$newVersion -t nvrbox/autoschedule:latest -f ./SchDockerfile . --no-cache
 
 # Docker Hub'a push et
 docker push nvrbox/autobot:$newVersion
 docker push nvrbox/autobot:latest
-docker push nvrbox/autoschedul:$newVersion
-docker push nvrbox/autoschedul:latest
+docker push nvrbox/autoschedule:$newVersion
+docker push nvrbox/autoschedule:latest
 
 # Yeni versiyonu kaydet
-Set-Content -Path $versionFile -Value $newVersion
+# Set-Content -Path $versionFile -Value $newVersion
